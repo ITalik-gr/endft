d1e751322e02e8d6eb983596834d9169f52e86e4
Italik <vitalil.gricenko77@gmail.com>
2022-07-27 22:55:08 +0300

    add header desctop ver. complete image animate + taimer

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // конечная дата, например 1 июля 2021
+  const deadline = new Date(2023, 09, 28);
+  // id таймера
+  let timerId = null;
+  // склонение числительных
+  function declensionNum(num, words) {
+    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+  }
+  // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+  function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $hours.textContent = hours < 10 ? '0' + hours : hours;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+    $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+    $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+  }
+  // получаем элементы, содержащие компоненты даты
+  const $hours = document.querySelector('.header-img_text-5__hour_act');
+  const $minutes = document.querySelector('.header-img_text-5__minutes_act');
+  const $seconds = document.querySelector('.header-img_text-5__sec_act');
+  // вызываем функцию countdownTimer
+  countdownTimer();
+  // вызываем функцию countdownTimer каждую секунду
+  timerId = setInterval(countdownTimer, 1000);
+});
